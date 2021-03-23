dc963c4d28cdf2b1e7284271fbf002e89846d67d
friendlyanon <friendlyanon@users.noreply.github.com>
2021-03-24 00:29:11 +0100

    Introduce the developer mode variable
    This variable will be used to cleanly separate parts of the project CML
    that are intended for the developers of the project from the parts that
    are also necessary for the consumers of the project, such as other
    developers using the project as a dependency or package maintainers
    building the project.
    
    This variable will allow shedding the superbuild CML and the use of a
    single CML as an entrypoint for the build.

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -4,6 +4,15 @@ string(
     PROJECT_IS_TOP_LEVEL
 )
 
+# ---- Developer mode ----
+
+if(PROJECT_IS_TOP_LEVEL)
+  option(example_DEVELOPER_MODE "Enable developer mode" OFF)
+  if("$ENV{CI}")
+    set(example_DEVELOPER_MODE ON CACHE INTERNAL "")
+  endif()
+endif()
+
 # ---- Warning guard ----
 
 # Protect dependents from this project's warnings if the guard isn't disabled
